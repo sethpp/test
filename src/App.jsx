df7d8992726473e863df3dc6d4bd2b37fa3c0511
piseth <pisethlongkoeun@gmail.com>
2025-01-08 13:48:39 +0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AnimeNavbar from './Components/AnimeNavbar';
+import AnimeHome from './Components/AnimeHome';
+import AnimePopular from './Components/AnimePopular';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <AnimeNavbar />
+      <Routes>
+        <Route path="/" element={<AnimeHome />} />
+        <Route path="/popular" element={<AnimePopular />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
